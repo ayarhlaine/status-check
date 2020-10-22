ee5ecdb98d5ebe9b718e3e9a6fbef90b71769d62
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-10-23 00:44:49 +0630

    change status name

--- sendStatus.sh
+++ sendStatus.sh
@@ -7,4 +7,4 @@ echo $TARGET_URL
 curl "https://api.github.com/repos/ayarhlaine/status-check/statuses/"$COMMIT_SHA \
 -H "Content-Type: application/json" \
 -H "Authorization: token $GITHUB_TOKEN" \
--X POST -d "{\"state\": \"$STATE\", \"context\": \"continuous-integration/jenkins\", \"description\": \"$DESCRIPTION\", \"target_url\": \"$TARGET_URL\"}"
+-X POST -d "{\"state\": \"$STATE\", \"context\": \"jenkins/unit-test\", \"description\": \"$DESCRIPTION\", \"target_url\": \"$TARGET_URL\"}"
