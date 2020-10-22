d5446584ddb84b5c1243a4ec0c44ae1b905af55b
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-10-23 00:38:01 +0630

    Update sendStatus.sh

--- sendStatus.sh
+++ sendStatus.sh
@@ -4,7 +4,7 @@ echo $GITHUB_TOKEN
 echo $STATE
 echo $DESCRIPTION
 echo $TARGET_URL
-curl "https://api.github.com/repos/ayarhlaine/status-check/statuses/$COMMIT_SHA" \
+curl "https://api.github.com/repos/ayarhlaine/status-check/statuses/"$COMMIT_SHA \
 -H "Content-Type: application/json" \
 -H "Authorization: token $GITHUB_TOKEN" \
 -X POST -d "{\"state\": \"$STATE\", \"context\": \"continuous-integration/jenkins\", \"description\": \"$DESCRIPTION\", \"target_url\": \"$TARGET_URL\"}"
